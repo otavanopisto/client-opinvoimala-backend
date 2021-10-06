5f4bb2e949a58bf896c469a4a529f6c500c3cb16
WeceW <toni.weckroth@gmail.com>
2021-10-06 09:27:30 +0300

    COPY-PASTED forgotPassword controller from node_modules to be customized here

--- app/extensions/users-permissions/controllers/Auth.js
+++ app/extensions/users-permissions/controllers/Auth.js
@@ -348,4 +348,117 @@ module.exports = {
       ctx.badRequest(null, formatError(adminError));
     }
   },
+
+  async forgotPassword(ctx) {
+    const usersPermissionsService =
+      strapi.plugins["users-permissions"].services.userspermissions;
+
+    const emailService = strapi.plugins["email"].services.email;
+
+    const email = ctx.request.body.email?.toLowerCase();
+
+    // Check if the provided email is valid or not.
+    const isEmail = emailRegExp.test(email);
+
+    if (!isEmail) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "Auth.form.error.email.format",
+          message: "Please provide a valid email address.",
+        })
+      );
+    }
+
+    const pluginStore = await strapi.store({
+      environment: "",
+      type: "plugin",
+      name: "users-permissions",
+    });
+
+    // Find the user by email.
+    const user = await strapi
+      .query("user", "users-permissions")
+      .findOne({ email });
+
+    // User not found.
+    if (!user) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "Auth.form.error.user.not-exist",
+          message: "This email does not exist.",
+        })
+      );
+    }
+
+    // User blocked
+    if (user.blocked) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "Auth.form.error.user.blocked",
+          message: "This user is disabled.",
+        })
+      );
+    }
+
+    // Generate random token.
+    const resetPasswordToken = crypto.randomBytes(64).toString("hex");
+
+    const settings = await pluginStore
+      .get({ key: "email" })
+      .then((storeEmail) => {
+        try {
+          return storeEmail["reset_password"].options;
+        } catch (error) {
+          return {};
+        }
+      });
+
+    const advanced = await pluginStore.get({
+      key: "advanced",
+    });
+
+    const userInfo = sanitizeEntity(user, {
+      model: strapi.query("user", "users-permissions").model,
+    });
+
+    settings.message = await usersPermissionsService.template(
+      settings.message,
+      {
+        URL: advanced.email_reset_password,
+        USER: userInfo,
+        TOKEN: resetPasswordToken,
+      }
+    );
+
+    settings.object = await usersPermissionsService.template(settings.object, {
+      USER: userInfo,
+    });
+
+    try {
+      // Send an email to the user.
+      await emailService.send({
+        to: user.email,
+        from:
+          settings.from.email || settings.from.name
+            ? `${settings.from.name} <${settings.from.email}>`
+            : undefined,
+        replyTo: settings.response_email,
+        subject: settings.object,
+        text: settings.message,
+        html: settings.message,
+      });
+    } catch (err) {
+      return ctx.badRequest(null, err);
+    }
+
+    // Update the user.
+    await strapi
+      .query("user", "users-permissions")
+      .update({ id: user.id }, { resetPasswordToken });
+
+    ctx.send({ ok: true });
+  },
 };
