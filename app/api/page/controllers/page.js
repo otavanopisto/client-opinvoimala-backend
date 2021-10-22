0a1737c67a7abd71e74436104333a0a21fb54d74
WeceW <toni.weckroth@gmail.com>
2021-10-22 15:47:49 +0300

    OPI-31: Return content pages only for allowed user roles

--- app/api/page/controllers/page.js
+++ app/api/page/controllers/page.js
@@ -1,8 +1,58 @@
-'use strict';
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizedPage = (page) => {
+  delete page.users_permissions_roles;
+  return sanitizeEntity(page, { model: strapi.models.page });
+};
+
+const errorResponse = (ctx, errors, type) => {
+  switch (type) {
+    case "forbidden":
+      return ctx.forbidden();
+    default:
+      return ctx.badRequest(null, [{ messages: errors }]);
+  }
+};
+
+const isPublicPage = (page) => {
+  const roles = page.users_permissions_roles;
+  const roleTypes = roles.map(({ type }) => type);
+
+  return !roles.length || roleTypes.includes("public");
+};
+
+const isUserAllowed = (user, page) => {
+  const roles = page.users_permissions_roles;
+  const roleIds = roles.map(({ id }) => id);
+
+  // Page is public if no roles are set
+  if (!roles.length) return true;
+
+  // No user or user has no role -> not allowed
+  if (!user?.role) return false;
+
+  // Allowed if user's role match to ones defined for the page
+  return roleIds.includes(user.role.id);
+};
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const user = ctx.state.user;
+
+    const page = await strapi.services.page.findOne({ id });
+
+    if (isPublicPage(page) || isUserAllowed(user, page)) {
+      return sanitizedPage(page);
+    }
+
+    return errorResponse(ctx, [], "forbidden");
+  },
+};
