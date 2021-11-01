ee6cabf520b2ae4fae5987da625827cfdebd733d
WeceW <toni.weckroth@gmail.com>
2021-11-01 15:23:14 +0200

    OPI-37: Handle slugs for tests

--- app/api/test/models/test.js
+++ app/api/test/models/test.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const { createSlug } = require("../../../utils/string");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (!data.slug) {
+        data.slug = createSlug(data.name);
+      } else {
+        data.slug = createSlug(data.slug);
+      }
+    },
+
+    beforeUpdate: async (params, data) => {
+      if (!data.slug) {
+        data.slug = createSlug(data.name);
+      } else {
+        data.slug = createSlug(data.slug);
+      }
+    },
+  },
+};
