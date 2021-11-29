3ff64a13b37a8fdfed2c20dda4b79bc52862418e
Toni Weckroth <toni.weckroth@geniem.com>
2021-11-29 11:54:17 +0200

    Feature/opi 20 user profile (#18)
    * OPI-53: Endpoint to return user's tests summary/feedback (#16)
    
    * OPI-53: Endpoint to return user's tests summary/feedback
    
    * OPI-53: Sanitize image data
    
    * OPI-53: Sort categories
    
    * OPI-53: Round stars to one decimal
    
    * OPI-20: Test category can be shown/hidden in user's profile
    
    * OPI-20: Include priority field to simple test

--- app/api/test/utils.js
+++ app/api/test/utils.js
@@ -145,16 +145,14 @@ module.exports = {
   async getSummaryText(stars) {
     const tests_summary = await strapi.services["tests-summary"].find();
 
-    if (tests_summary?.thresholds) {
-      for (const threshold of tests_summary.thresholds) {
-        const aboveMin = stars >= threshold.min_stars;
-        const belowMax = stars <= threshold.max_stars;
-        if (aboveMin && belowMax) {
-          return {
-            summary_text: threshold.summary,
-            details_text: threshold.details,
-          };
-        }
+    for (const threshold of tests_summary.thresholds) {
+      const aboveMin = stars >= threshold.min_stars;
+      const belowMax = stars <= threshold.max_stars;
+      if (aboveMin && belowMax) {
+        return {
+          summary_text: threshold.summary,
+          details_text: threshold.details,
+        };
       }
     }
     return {
