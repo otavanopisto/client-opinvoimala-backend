047f3011140015cce03ae310afe8fd1c2c228a06
Petja Laitila <petja.laitila@otavanopisto.fi>
2023-05-11 11:12:23 +0300

    Nodemailer plugin config correction

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,19 +1,17 @@
 module.exports = ({ env }) => ({
   email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.example.com'),
-        port: env('SMTP_PORT', 587),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
-        },
-      },
-      settings: {
-        defaultFrom: 'no-reply@opinvoimala.fi',
-        defaultReplyTo: 'no-reply@opinvoimala.fi',
+    provider: 'nodemailer',
+    providerOptions: {
+      host: env('SMTP_HOST'),
+      port: env('SMTP_PORT'),
+      auth: {
+        user: env('SMTP_USERNAME'),
+        pass: env('SMTP_PASSWORD'),
       },
     },
+    settings: {
+      defaultFrom: 'no-reply@opinvoimala.fi',
+      defaultReplyTo: 'no-reply@opinvoimala.fi',
+    },
   },
-})
+});
