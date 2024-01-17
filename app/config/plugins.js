1a8c3b477ace42466464d0a71c75fcd174adb536
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2024-01-17 12:05:33 +0200

    Meilisearch

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,17 +1,23 @@
 module.exports = ({ env }) => ({
   email: {
-    provider: 'nodemailer',
+    provider: "nodemailer",
     providerOptions: {
-      host: env('SMTP_HOST'),
-      port: env('SMTP_PORT'),
+      host: env("SMTP_HOST"),
+      port: env("SMTP_PORT"),
       auth: {
-        user: env('SMTP_USERNAME'),
-        pass: env('SMTP_PASSWORD'),
+        user: env("SMTP_USERNAME"),
+        pass: env("SMTP_PASSWORD"),
       },
     },
     settings: {
-      defaultFrom: 'no-reply@opinvoimala.fi',
-      defaultReplyTo: 'no-reply@opinvoimala.fi',
+      defaultFrom: "no-reply@opinvoimala.fi",
+      defaultReplyTo: "no-reply@opinvoimala.fi",
+    },
+    meilisearch: {
+      config: {
+        host: env("MEILI_HOST"),
+        apiKey: env("MEILI_MASTER_KEY"),
+      },
     },
   },
 });
